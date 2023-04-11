57c0b1d9cdbd2e70e2e25490fcaa9c009b7e268e
kapoor-prateek <kapoor-prateek@users.noreply.github.com>
2023-04-11 11:55:23 +0100

    VEOSS-1390: Update SBT

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "webdriver-factory"  % "0.25.0"   % Test,
+    "uk.gov.hmrc"         %% "webdriver-factory"  % "0.43.0"   % Test,
     "org.scalatest"       %% "scalatest"          % "3.2.12"   % Test,
     "org.scalatestplus"   %% "selenium-3-141"     % "3.2.10.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"       % "0.64.0"   % Test,
@@ -23,8 +23,7 @@ object Dependencies {
     "junit"                % "junit"              % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"    % "0.11"     % Test,
     "com.typesafe"         % "config"             % "1.4.2"    % Test,
-    "org.mongodb.scala"   %% "mongo-scala-driver" % "4.6.0"    % Test,
-    "com.typesafe.play"   %% "play-iteratees"     % "2.6.1"    % Test
+    "org.mongodb.scala"   %% "mongo-scala-driver" % "4.7.1"    % Test
   )
 
 }
