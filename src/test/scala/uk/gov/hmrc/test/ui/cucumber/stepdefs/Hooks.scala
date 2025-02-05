eba815ebb30a2fc5f42b5edb290c3720a9d02b15
Susan Badel <susan.badel@opencastsoftware.com>
2025-02-05 11:33:21 +0000

    VEOSS-1966 more returns api data updates

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
@@ -31,8 +31,8 @@ object Hooks extends ScalaDsl with EN with Browser {
   }
 
   private def resetAll(): Unit = {
-    MongoConnection.dropReturns()
-    MongoConnection.dropCorrections()
+//    MongoConnection.dropReturns()
+//    MongoConnection.dropCorrections()
     MongoConnection.dropSavedAnswers()
     MongoConnection.dropCachedVatReturns()
     MongoConnection.dropCachedRegistrations()
